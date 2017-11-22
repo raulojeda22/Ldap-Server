cdce30b9906524392c8bd173c87ec7895f7a673b
raulojeda22 <32483063+raulojeda22@users.noreply.github.com>
2017-11-22 10:10:41 +0100

    Add files via upload

--- backup.sh
+++ backup.sh
@@ -3,12 +3,14 @@ onEstic=`pwd`
 myIP=`ifconfig enp0s3 | grep "inet addr:" | cut -d" " -f12 | cut -d":" -f2`
 echo -n "Original Server IP: "
 read originalIP
-echo ""
+echo -n "Your user name: "
+read replicaUser
 echo -n "Original Server User: "
 read userName
 ssh -t $userName@$originalIP sudo /etc/init.d/slapd stop
+#ssh -t $userName@$originalIP touch backup-ldap.ldif
 #ssh -t $userName@$originalIP sudo slapcat > backup-ldap.ldif
-ssh -t $userName@$originalIP sudo scp backup-ldap.ldif $USER@$myIP:$onEstic
+ssh -t $userName@$originalIP sudo scp backup-ldap.ldif $replicaUser@$myIP:$onEstic
 ssh -t $userName@$originalIP sudo /etc/init.d/slapd start
 sudo chmod 777 backup-ldap.ldif
 sudo /etc/init.d/slapd stop
